fb0b389d8bb2aa1bb7facf38e1b0c21827f92429
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-08-19 01:37:03 +0530

    add clerk auth and build sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,31 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-// import {
-//   ChatBox,
-//   Connections,
-//   CreatePost,
-//   Discover,
-//   Feed,
-//   Login,
-//   Messages,
-//   Profile
-// } from './pages';
-
+import Feed from './pages/Feed';
+import {useUser} from '@clerk/clerk-react'
+import Layout from './pages/Layout';
+import Profile from './pages/Profile';
+import Connections from './pages/Connections';
+import Messages from './pages/Messages';
+import ChatBox from './pages/ChatBox';
+import Discover from './pages/Discover';
+import CreatePost from './pages/CreatePost';
 
 const App = () => {
+  const {user} = useUser();
   return (
     <>
         <Routes>
-          <Route path="/" element={<Login />} />
-          {/* <Route index element={<Feed />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:profileId" element={<Profile />} />
-          <Route path="/connections" element={<Connections />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/messages/:userId" element={<ChatBox />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/create-post" element={<CreatePost />} /> */}
+          <Route path="/" element={!user ? <Login /> : <Layout />}>
+            <Route index element={<Feed />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="profile/:profileId" element={<Profile />} />
+            <Route path="connections" element={<Connections />} />
+            <Route path="messages" element={<Messages />} />
+            <Route path="messages/:userId" element={<ChatBox />} />
+            <Route path="discover" element={<Discover />} />
+            <Route path="create-post" element={<CreatePost />} />
+          </Route>
         </Routes>
     </>
   );
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading';
 
 const Feed = () => {
-  return (
-    <div>
-      
+
+  const [FeedData, setFeedData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  const fetchFeedData = async () => {
+    setFeedData(dummyPostsData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchFeedData();
+  }, []);
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* stories and post list */}
+      <div>
+        <h1>Stories here</h1>
+        <div className='p-4 space-y-6'>
+          List of post
+        </div>
+      </div>
+
+      {/* right sidebar */}
+      <div>
+
+      </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default Feed
