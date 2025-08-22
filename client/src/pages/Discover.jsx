c6a408b07a2f1832df27f07bc32c357e71cba4ac
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-08-22 06:04:37 +0530

    feat: implement user profile editing and discovery features

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import { dummyConnectionsData } from '../assets/assets';
+import { Search } from 'lucide-react';
+import UserCard from '../components/UserCard';
+import Loading from '../components/Loading';
 
 const Discover = () => {
+
+  const [inputValue, setInputValue] = React.useState('');
+  const [users, setUsers] = React.useState(dummyConnectionsData);
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSearch = async(e) => {
+    if(e.key === 'Enter') {
+      setUsers([]);
+      setLoading(true);
+      setTimeout(() => {
+        setUsers(dummyConnectionsData);
+        setLoading(false);
+      }, 1000);
+    }
+  }
+
   return (
-    <div>
-      
+    <div className='min-h-screen bg-gradient-to-b from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+         
+         {/* title */}
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Discover People</h1>
+          <p>Connect with amazing people and grow your network</p>
+        </div>
+
+        {/* search bar */}
+        <div className='mb-8 shadow-md rounded-md border border-slate-200/60 bg-white/80'>
+          <div className='p-6'>
+            <div className='relative'>
+              <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400 w-5 h-5' />
+              <input onChange={(e) => setInputValue(e.target.value)} value={inputValue} onKeyUp={handleSearch} type="text" name="" placeholder='Search people by name, username....' className='pl-10 sm:pl-12 py-2 w-full border border-gray-300 rounded-md max-sm:text-sm' id="" />
+            </div>
+          </div>
+        </div>
+
+        {/* user section */}
+        <div className='flex flex-wrap gap-6'>
+          {users.map((user) => (
+            <UserCard key={user._id} user={user} />
+          ))}
+        </div>
+
+        {
+          loading && (<Loading height='60vh' />)
+        }
+
+      </div>
     </div>
   )
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom';
+import { dummyPostsData, dummyUserData } from '../assets/assets';
+import Loading from '../components/Loading';
+import UserProfileInfo from '../components/UserProfileInfo';
+import PostCard from '../components/PostCard';
+import moment from 'moment';
+import ProfileModel from '../components/ProfileModel';
 
 const Profile = () => {
-  return (
-    <div>
-      
+  const {profileId} = useParams();
+
+  const [user, setUser] = React.useState(null);
+  const [posts, setPosts] = React.useState([]);
+  const [active, setActive] = React.useState('post');
+  const [showEdit, setShowEdit] = React.useState(false);
+
+  const fetchUserData = async () => {
+    setUser(dummyUserData);
+    setPosts(dummyPostsData);
+  }
+
+  useEffect(() => {
+    fetchUserData();
+  }, [profileId]);
+
+  return user ? (
+    <div className='relative h-full overflow-y-scroll bg-gray-50 p-6'>
+      <div className='max-w-3xl mx-auto'>
+        
+        {/* Profile card */}
+        <div className='bg-white rounded-2xl shadow overflow-hidden'>
+          {/* cover photo */}
+          <div className='h-40 md:h-56 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200'>
+            {user.cover_photo && <img src={user.cover_photo} alt='Cover Photo' className='w-full h-full object-cover' />}
+          </div>
+
+          {/* user info */}
+          <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit} />
+        </div>
+        
+        {/* tabs */}
+        <div className='mt-6'>
+          <div className='bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
+            {['post', 'media', 'about'].map((tab) => (
+              <button
+                key={tab}
+                onClick={() => setActive(tab)}
+                className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${
+                  active === tab ? 'bg-indigo-600 text-white' : 'hover:text-gray-900 text-gray-900'
+                }`}
+              >
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+              </button>
+            ))}
+          </div>
+
+          {/* list of posts */}
+          {active === 'post' && (
+            <div className='mt-6 flex flex-col items-center gap-6'>
+              {posts.map((post) => <PostCard key={post._id} post={post} />)}
+            </div>
+          )}
+
+          {/* list of media */}
+          {active === 'media' && (
+            <div className='flex flex-wrap mt-6 max-w-6xl'>
+              {
+                posts.filter((post) => post.image_urls.length > 0).map((post) => (
+                  <>
+                    {post.image_urls.map((url, index) => (
+                      <Link target='_blank' to={url} key={index} className='relative group'>
+                        <img src={url} key={url} className='w-64 aspect-video object-cover' alt='' />
+                        <p className='absolute bottom-0 right-0 text-xs p-1 px-3 backdrop-blur-xl text-white opacity-0 group-hover:opacity-100 transition duration-300'>Posted {moment(post.createdAt).fromNow()}</p>
+                      </Link>
+                    ))}
+                  </>
+                ))}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* edit profile tool */}
+      {showEdit && <ProfileModel setShowEdit={setShowEdit} />}
     </div>
-  )
+  ) : (<Loading />);
 }
 
 export default Profile
