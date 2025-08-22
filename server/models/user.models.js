a239ecb79b1ee19c14b6372cca88de79928e5bd5
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-08-23 03:19:35 +0530

    refactor: remove redundant 'name' field from user schema

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -2,10 +2,6 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   _id: {type: String, required: true},
-  name: {
-    type: String,
-    required: true,
-  },
   full_name: {type: String, required: true},
   username: {type: String, required: true}, 
   bio: {type: String, default: ""},
