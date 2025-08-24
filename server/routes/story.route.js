b2aefac07bc25d9a9223212239fcf7abcafcadbf
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-08-25 02:24:09 +0530

    fix: Correct import statement for story controller and ensure proper export of storyRouter

--- server/routes/story.route.js
+++ server/routes/story.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { upload } from '../config/multar.js';
 import { protect } from '../middleware/auth.js';
-import { addUserStory, getUserStories } from '../controllers/story.controller';
+import { addUserStory, getUserStories } from '../controllers/story.controller.js';
 
 const storyRouter = express.Router();
 
 storyRouter.post('/create', upload.single('media'), protect, addUserStory);
 storyRouter.get('/get', protect, getUserStories);
+
+export default storyRouter;
