13dde21e53e2c47e2a95f1013d336144d612cd46
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-08-26 02:43:06 +0530

    feat: Add user profile route to user router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updateUserData } from '../controllers/user.controller.js';
+import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, getUserProfile, sendConnectionRequest, unfollowUser, updateUserData } from '../controllers/user.controller.js';
 import {protect} from '../middleware/auth.js'
 import { upload } from '../config/multar.js';
 import { getUserRecentMessages } from '../controllers/message.controller.js';
@@ -13,6 +13,7 @@ userRouter.post('/unfollow', protect, unfollowUser);
 userRouter.post('/connect', protect, sendConnectionRequest);
 userRouter.post('/accept', protect, acceptConnectionRequest);
 userRouter.get('/connections', protect, getUserConnections);
+userRouter.post('/profile', protect, getUserProfile);
 userRouter.get('/profiles', getUserData);
 userRouter.get('/recent-messages', protect, getUserRecentMessages);
 
