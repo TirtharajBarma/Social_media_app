69e47dee1ae71db3635e468278c444335bb4801c
TIRTHARAJ BARMA <119959056+TirtharajBarma@users.noreply.github.com>
2025-08-30 01:55:13 +0530

    fix: clerk login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import { Star } from 'lucide-react'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignIn } from '@clerk/clerk-react';
 
 const Login = () => {
   return (
@@ -19,25 +19,18 @@ const Login = () => {
               <div className='flex'>
                 {Array(5).fill(0).map((_, i) => (<Star key={i}  className='size-4 md:size-4.5 text-transparent fill-amber-500'/>))}
               </div>
-              <p>User by 12k+ developers</p>
+              <p>Used by many friends</p>
             </div>
           </div>
           <h1 className='text-3xl md:text-6xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent'>More than just friends truly connect</h1>
-          <p className='text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md'>connect with global community on Chattrix</p>
+          <p className='text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md'>connect on Chattrix</p>
         </div>
         <span className='md:h-10'></span>
       </div>
       
       {/* right side: login form */}
       <div className='flex-1 flex items-center justify-center p-6 sm:p-10'>
-        <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+        <SignIn />
       </div>
     </div>
   )
